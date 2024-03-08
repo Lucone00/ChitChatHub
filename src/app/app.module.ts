b59d3789016fa7cedcc47fb30f79b900f6dbfb71
Lucone00 <luca.giuntoli2001.b@gmail.com>
2024-03-08 10:36:29 +0000

    creato componente container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatContainerComponent } from './features/chat-container/chat-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatContainerComponent
   ],
   imports: [
     BrowserModule,
